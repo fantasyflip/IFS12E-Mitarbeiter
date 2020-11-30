8c2ca9f365c8b2bb656c02204e551faff1309e36
fantasyflip <katzenberger.ph+gitgub@gmail.com>
2020-11-30 09:12:54 +0100

    Klassen inkl. Attribute und Methoden geschrieben

--- Mitarbeiter/Mitarbeiter.cs
+++ Mitarbeiter/Mitarbeiter.cs
@@ -6,11 +6,89 @@ using System.Text;
 namespace Mitarbeiter
 {
     // abstrakte Basisklasse 
-    public abstract class T_Mitarbeiter 
+    public abstract class CMitarbeiter 
     {
+        private String Vorname;
+        private String Nachname;
 
-    };
+        public void setVorname(String neuVorname)
+        {
+            this.Vorname = neuVorname;
+        }
+
+        public String getVorname()
+        {
+            return this.Vorname;
+        }
+
+        public void setNachname(String neuNachname)
+        {
+            this.Nachname = neuNachname;
+        }
+
+        public String getNachname()
+        {
+            return this.Nachname;
+        }
+    }
 
     // Weitere Klassen
+    public class CAngestellter : CMitarbeiter
+    {
+        private double Bruttogehalt;
+
+        public CAngestellter()
+        {
+
+        }
+
+        public CAngestellter(String neuVorname, String neuNachname, double neuBrutto)
+        {
+            this.setVorname(neuVorname);
+            this.setNachname(neuNachname);
+            this.setBrutto(neuBrutto);
+        }
+
+        public void setBrutto(double neuBrutto)
+        {
+            this.Bruttogehalt = neuBrutto;
+        }
+
+        public double getBrutto()
+        {
+            return this.Bruttogehalt;
+        }
+    }
+
+    public class CArbeiter : CMitarbeiter
+    {
+        private double Stundenzahl;
+        private double Stundenlohn;
+
+        public CArbeiter()
+        {
+
+        }
+
+        public CArbeiter(String neuVorname, String Nachname, double Stundenzahl, double Stundenlohn)
+        {
+
+        }
+
+        public double getBrutto()
+        {
+            return this.Stundenlohn * this.Stundenzahl;
+        }
+
+        public void setStundenzahl(double neuAnzahl)
+        {
+            this.Stundenzahl = neuAnzahl;
+        }
+
+        public void setStundenlohn(double neuLohn)
+        {
+            this.Stundenlohn = neuLohn;
+        }
+    }
 
 }
